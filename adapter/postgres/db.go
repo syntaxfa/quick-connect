48f2de49149712895ce28f4a7c6384bd91012d0f
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-05-01 21:28:33 +0330

    feat(adapter): add postgres (#32)

---
+++ adapter/postgres/db.go
@@ -0,0 +1,99 @@
+package postgres
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"github.com/syntaxfa/quick-connect/pkg/errlog"
+	"github.com/syntaxfa/quick-connect/pkg/logger"
+	"github.com/syntaxfa/quick-connect/pkg/richerror"
+	"log/slog"
+	"sync"
+	"time"
+)
+
+type Database struct {
+	db         *sql.DB
+	mu         sync.Mutex
+	statements map[statementKey]*sql.Stmt
+}
+
+func New(cfg Config) *Database {
+	op := "repository.postgres.New"
+
+	conn, err := sql.Open("postgres",
+		fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
+			cfg.Host, cfg.Port, cfg.Username, cfg.Password, cfg.DBName, cfg.SSLMode))
+	if err != nil {
+		logger.L().Error("Connection to postgres failed", slog.String("error", err.Error()))
+	}
+
+	if err = conn.Ping(); err != nil {
+		richErr := richerror.New(op).WithWrapError(err).WithKind(richerror.KindUnexpected)
+		errlog.ErrLog(richErr)
+
+		panic(richErr.Message())
+	}
+
+	conn.SetMaxIdleConns(cfg.MaxIdleConns)
+	conn.SetMaxOpenConns(cfg.MaxOpenConns)
+	conn.SetConnMaxLifetime(time.Duration(cfg.ConnMaxLifetime) * time.Second)
+
+	logger.L().Info("postgres connection established successfully!!!")
+
+	return &Database{
+		db:         conn,
+		statements: make(map[statementKey]*sql.Stmt),
+	}
+}
+
+func (db *Database) Conn() *sql.DB {
+	return db.db
+}
+
+func (db *Database) PrepareStatement(ctx context.Context, key statementKey, query string) (*sql.Stmt, error) {
+	const op = "repository.postgres.PrepareStatement"
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	if stmt, ok := db.statements[key]; ok {
+		return stmt, nil
+	}
+
+	stmt, pErr := db.db.PrepareContext(ctx, query) //nolint:sqlclosecheck // this is closed in CloseStatements method.
+	if pErr != nil {
+		richErr := richerror.New(op).WithWrapError(pErr).WithKind(richerror.KindUnexpected).
+			WithMeta(map[string]interface{}{"key": key, "query": query})
+		errlog.ErrLog(richErr)
+
+		return nil, richErr
+	}
+
+	db.statements[key] = stmt
+
+	return stmt, nil
+}
+
+func (db *Database) CloseStatements() error {
+	const op = "repository.postgres.CloseStatement"
+
+	db.mu.Lock()
+	defer db.mu.Unlock()
+
+	for key, stmt := range db.statements {
+		cErr := stmt.Close()
+		if cErr != nil {
+			richErr := richerror.New(op).WithWrapError(cErr).WithMeta(map[string]interface{}{"key": key})
+			errlog.ErrLog(richErr)
+
+			return richErr
+		}
+	}
+
+	return nil
+}
+
+func (db *Database) Close() error {
+	return db.db.Close()
+}
