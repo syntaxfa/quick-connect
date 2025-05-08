f2c234adb17e93dcaa55c453c8a513adc3b57935
alireza-fa <alirezafeyze44@gmail.com>
2025-05-08 17:26:35 +0330

    feat(action): add chat-test action

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -12,7 +12,7 @@ import (
 
 type Config struct {
 	FilePath         string `koanf:"file_path"`
-	UseLocalTime     bool   `koanf:"use_local_tim"`
+	UseLocalTime     bool   `koanf:"use_local_time"`
 	FileMaxSizeInMB  int    `koanf:"file_max_size_in_mb"`
 	FileMaxAgeInDays int    `koanf:"file_max_age_in_days"`
 	MaxBackup        int    `koanf:"max_backup"`
