31c235cf298122812abfdbd9990858810410b0fe
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-05-14 13:30:31 +0330

    feat(pkg): migrate from goose to sql migrate (#89)
    * feat(pkg): migrate from goose to sql-migrate
    
    * feat(example): add migrator example

---
+++ example/migrator/migrations/1744720310_updated_at_column.sql
@@ -0,0 +1,12 @@
+-- +migrate StatementBegin
+CREATE OR REPLACE FUNCTION update_updated_at_column()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+-- +migrate StatementEnd
+
+-- +migrate Down
+DROP FUNCTION IF EXISTS update_updated_at_column();
