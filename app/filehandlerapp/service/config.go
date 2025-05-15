33bf24a9485921b692a4c3bd679e757652227b02
amirhossein <amirhosseinmobini9@gmail.com>
2025-05-15 17:12:57 +0330

    feat(filehandler): file handler config added

---
+++ app/filehandlerapp/service/config.go
@@ -0,0 +1,17 @@
+package service
+
+type S3Config struct {
+	BucketName string `koanf:"bucket_name"`
+	Region     string `koanf:"region"`
+	S3Key      string `koanf:"s3_key"`
+}
+
+type LocalStorageConfig struct {
+	BaseDir string `koanf:"base_dir"`
+}
+
+type Config struct {
+	StorageType string             `koanf:"storage_type"`
+	Local       LocalStorageConfig `koanf:"local"`
+	S3          S3Config           `koanf:"s3"`
+}
