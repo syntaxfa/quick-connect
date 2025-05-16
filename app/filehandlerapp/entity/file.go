8581e6dc033e3799272609c1797a95f49cd0b821
amirhossein mobini <105078726+TheAmirhosssein@users.noreply.github.com>
2025-05-17 01:44:19 +0330

    feat(filehandler): implement file handler entity design (#102)
    * feat(filehandler): file handler entities added
    
    * feat(filehandler): file handler config added
    
    * feat(filehandler): some fields added into file entity
    
    * feat(filehandler): some files formatted

---
+++ app/filehandlerapp/entity/file.go
@@ -0,0 +1,32 @@
+package entity
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/syntaxfa/quick-connect/types"
+)
+
+const (
+	FileTypeChat = "chat"
+
+	FileStorageTypeLocal = "local"
+	FileStorageTypeS3    = "s3"
+)
+
+type File struct {
+	ID          types.ULID
+	Type        string
+	TypeID      types.ID
+	Uploaded    time.Time
+	Extension   string
+	StorageType string
+	Size        int // in megabytes
+	ContentType string
+	IsPublic    bool
+	IsDeleted   bool
+}
+
+func (f *File) GetFileName() string {
+	return fmt.Sprintf("%v.%v", f.ID, f.Extension)
+}
--- types/id.go
+++ types/id.go
@@ -1,3 +1,5 @@
 package types
 
 type ID uint64
+
+type ULID string
