1046b2c7f01d236c5be098ee1a12b2e534d26ac5
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-05-18 18:50:56 +0330

    feat(quick-conn): setting up development environment (#109)
    * feat(quick-conn): update gitignore
    
    * feat(quick-conn): add chat service air config
    
    * fix(file): fix import error
    
    * feat(chat): development dockerfile
    
    * feat(air): add manager air config
    
    * feat(manager): development dockerfile
    
    * feat(quick-conn): development docker compose

--- app/filehandlerapp/service/config.go
+++ app/filehandlerapp/service/config.go
@@ -1,10 +1,13 @@
 package service
 
-import "github.com/syntaxfa/quick-connect/adapter/file"
+import (
+	"github.com/syntaxfa/quick-connect/adapter/aws"
+	"github.com/syntaxfa/quick-connect/adapter/file"
+)
 
 type Config struct {
 	StorageType        string      `koanf:"storage_type"`
 	TempDeleteDuration int         `koanf:"temp_delete_duration"`
 	Local              file.Config `koanf:"local"`
-	S3                 S3Config    `koanf:"s3"`
+	S3                 aws.Config  `koanf:"s3"`
 }
