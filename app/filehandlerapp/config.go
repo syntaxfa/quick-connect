f44b28c9321e45dc8a8111916d865af46074a601
amirhossein <amirhosseinmobini9@gmail.com>
2025-05-22 23:44:20 +0330

    refactor(filehandler): file handler config file edited

--- app/filehandlerapp/config.go
+++ app/filehandlerapp/config.go
@@ -4,14 +4,12 @@ import (
 	"github.com/syntaxfa/quick-connect/adapter/aws"
 	"github.com/syntaxfa/quick-connect/adapter/file"
 	"github.com/syntaxfa/quick-connect/adapter/postgres"
-	"github.com/syntaxfa/quick-connect/app/managerapp/service/tokenservice"
 	"github.com/syntaxfa/quick-connect/pkg/logger"
 )
 
 type Config struct {
-	Logger   logger.Config       `koanf:"logger"`
-	Token    tokenservice.Config `koanf:"token"`
-	Postgres postgres.Config     `koanf:"postgres"`
-	Local    file.Config         `koanf:"local"`
-	S3       aws.Config          `koanf:"s3"`
+	Logger   logger.Config   `koanf:"logger"`
+	Postgres postgres.Config `koanf:"postgres"`
+	Local    file.Config     `koanf:"local"`
+	S3       aws.Config      `koanf:"s3"`
 }
