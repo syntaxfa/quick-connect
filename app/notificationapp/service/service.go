f109b8a9fe4a02b4e661d588df29d0042e992546
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-06-11 15:28:18 +0330

    feat(notif): completed send notification service (#176)
    * feat(notif): external_users migrate
    
    * feat(notif): completed SendNotification service

--- app/notificationapp/service/service.go
+++ app/notificationapp/service/service.go
@@ -2,23 +2,36 @@ package service
 
 import (
 	"context"
+	"log/slog"
 
+	"github.com/syntaxfa/quick-connect/pkg/cachemanager"
 	"github.com/syntaxfa/quick-connect/types"
 )
 
 type Repository interface {
-	Save(ctx context.Context, notification Notification) error
+	Save(ctx context.Context, req SendNotificationRequest) (Notification, error)
 	FindByUserID(ctx context.Context, userID types.ID) ([]Notification, error)
 	MarkAsRead(ctx context.Context, notificationID types.ID) error
 	MarkAllAsReadByUserID(ctx context.Context, userID types.ID) error
+	IsExistUserIDFromExternalUserID(ctx context.Context, externalUserID string) (bool, error)
+	GetUserIDFromExternalUserID(ctx context.Context, externalUserID string) (types.ID, error)
+	CreateUserIDFromExternalUserID(ctx context.Context, externalUserID string, userID types.ID) error
 }
 
 type Service struct {
-	vld Validate
+	cfg    Config
+	vld    Validate
+	cache  *cachemanager.CacheManager
+	repo   Repository
+	logger *slog.Logger
 }
 
-func New(vld Validate) Service {
+func New(cfg Config, vld Validate, cache *cachemanager.CacheManager, repo Repository, logger *slog.Logger) Service {
 	return Service{
-		vld: vld,
+		cfg:    cfg,
+		vld:    vld,
+		cache:  cache,
+		repo:   repo,
+		logger: logger,
 	}
 }
---
+++ app/notificationapp/service/user_id.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/oklog/ulid/v2"
+	"github.com/syntaxfa/quick-connect/pkg/cachemanager"
+	"github.com/syntaxfa/quick-connect/types"
+)
+
+type UserIDCacheValue struct {
+	UserID types.ID `json:"user_id"`
+}
+
+func (s Service) getUserIDFromExternalUserID(ctx context.Context, externalUserID string) (types.ID, error) {
+	_, pErr := ulid.Parse(externalUserID)
+	if pErr == nil {
+		return types.ID(externalUserID), nil
+	}
+
+	key := s.getUserIDCacheKey(externalUserID)
+	var cacheValue UserIDCacheValue
+	if gErr := s.cache.Get(ctx, key, &cacheValue); gErr != nil {
+		if !errors.Is(gErr, cachemanager.ErrKeyNotFound) {
+			return "", gErr
+		}
+		exist, eErr := s.repo.IsExistUserIDFromExternalUserID(ctx, externalUserID)
+		if eErr != nil {
+			return "", eErr
+		}
+
+		if exist {
+			userID, gErr := s.repo.GetUserIDFromExternalUserID(ctx, externalUserID)
+			if gErr != nil {
+				return "", gErr
+			}
+
+			if sErr := s.cache.Set(ctx, key, UserIDCacheValue{UserID: userID}, s.cfg.UserIDCacheExpiration); sErr != nil {
+				return "", sErr
+			}
+
+			return userID, nil
+		}
+		userID := types.ID(ulid.Make().String())
+		if cErr := s.repo.CreateUserIDFromExternalUserID(ctx, externalUserID, userID); cErr != nil {
+			return "", cErr
+		}
+
+		if sErr := s.cache.Set(ctx, key, UserIDCacheValue{UserID: userID}, s.cfg.UserIDCacheExpiration); sErr != nil {
+			return "", sErr
+		}
+
+		return userID, nil
+	}
+
+	return cacheValue.UserID, nil
+}
+
+func (s Service) getUserIDCacheKey(externalUserID string) string {
+	return fmt.Sprintf("users:externals:%s", externalUserID)
+}
