b5046f6c54dac3e3aff3a6353f0936cbfd6a1b2c
alireza-fa <alirezafeyze44@gmail.com>
2025-07-06 22:57:26 +0330

    feat(example): text template & email template

---
+++ app/notificationapp/service/add_template.go
@@ -0,0 +1,13 @@
+package service
+
+import "context"
+
+func (s Service) AddTemplate(_ context.Context, _ AddTemplateRequest) (AddTemplateResponse, error) {
+	// validate request
+
+	// check name conflict
+
+	// insert to repository
+
+	return AddTemplateResponse{}, nil
+}
