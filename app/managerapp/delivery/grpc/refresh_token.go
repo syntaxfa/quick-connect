9f30e8229f666b9fea275c004a5ac7ccdbb6e6be
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-10-29 10:13:10 +0330

    feat(manager): token service `gRPC` (#312)
    * feat(manager): add TokenValidate gRPC
    
    * feat(manager): TokenRefresh gRPC

---
+++ app/managerapp/delivery/grpc/refresh_token.go
@@ -0,0 +1,22 @@
+package grpc
+
+import (
+	"context"
+
+	"github.com/syntaxfa/quick-connect/pkg/servermsg"
+	"github.com/syntaxfa/quick-connect/protobuf/manager/golang/authpb"
+)
+
+func (h Handler) TokenRefresh(_ context.Context, req *authpb.TokenRefreshRequest) (*authpb.TokenRefreshResponse, error) {
+	resp, sErr := h.tokenSvc.RefreshTokens(req.RefreshToken)
+	if sErr != nil {
+		return nil, servermsg.GRPCMsg(sErr, h.t, h.logger)
+	}
+
+	return &authpb.TokenRefreshResponse{
+		AccessToken:      resp.AccessToken,
+		RefreshToken:     resp.RefreshToken,
+		AccessExpiresIn:  resp.AccessExpiresIn,
+		RefreshExpiresIn: resp.RefreshExpireIn,
+	}, nil
+}
