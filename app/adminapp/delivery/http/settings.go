3e9583700987a847308caf06256bb71821e542d1
alireza feizi <86611004+alireza-fa@users.noreply.github.com>
2025-11-02 13:04:37 +0330

    fix(admin): dashboard panel load templates (#336)
    * fix(admin): url routing
    
    * fix(admin): main layout
    
    * feat(admin): add simple settings page

---
+++ app/adminapp/delivery/http/settings.go
@@ -0,0 +1,20 @@
+package http
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+// ShowSettingsPage renders the settings page
+func (h Handler) ShowSettingsPage(c echo.Context) error {
+	data := map[string]interface{}{
+		"Title":        "Settings Management",
+		"TemplateName": "settings_page",
+	}
+
+	if isHTMX(c) {
+		return c.Render(http.StatusOK, "settings_page", data)
+	}
+	return c.Render(http.StatusOK, "main_layout", data)
+}
