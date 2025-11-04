ccf8f766196e67e39ae4fe9ca22903ffe4807ce1
alireza-fa <alirezafeyze44@gmail.com>
2025-11-05 01:05:01 +0330

    feat(manager): add external_users database migaret

---
+++ app/managerapp/repository/migrations/1762291906_create_external_user.sql
@@ -0,0 +1,10 @@
+-- +migrate Up
+CREATE TABLE ID NOT EXISTS external_users (
+    "user_id" VARCHAR(26) NOT NULL UNIQUE,
+    "external_user_id" VARCHAR(255) NOT NULL UNIQUE
+);
+CREATE INDEX idx_external_user_id_external_user ON external_users(external_user_id);
+
+-- +migrate Down
+DROP INDEX idx_external_user_id_external_user;
+DROP TABLE IF EXISTS external_users;
