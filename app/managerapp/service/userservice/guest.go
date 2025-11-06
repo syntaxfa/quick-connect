21fe5acb5d90a50216aba89f97d6540d18348c85
alireza-fa <alirezafeyze44@gmail.com>
2025-11-06 14:47:32 +0330

    feat(manager): update guest user

--- app/managerapp/service/userservice/guest.go
+++ app/managerapp/service/userservice/guest.go
@@ -7,6 +7,7 @@ import (
 	"github.com/oklog/ulid/v2"
 	"github.com/syntaxfa/quick-connect/pkg/errlog"
 	"github.com/syntaxfa/quick-connect/pkg/richerror"
+	"github.com/syntaxfa/quick-connect/pkg/servermsg"
 	"github.com/syntaxfa/quick-connect/types"
 )
 
@@ -61,5 +62,68 @@ func (s Service) RegisterGuestUser(ctx context.Context, req RegisterGuestUserReq
 	return RegisterGuestUserResponse{
 		User:    user,
 		QCToken: qcToken,
+	}, nil
+}
+
+func (s Service) UpdateGuestUser(ctx context.Context, userID types.ID, req UpdateGuestUserRequest) (UpdateGuestUserResponse, error) {
+	const op = "service.guest.UpdateGuestUser"
+
+	if vErr := s.vld.ValidateUpdateGuestUserRequest(req); vErr != nil {
+		return UpdateGuestUserResponse{}, vErr
+	}
+
+	exists, existErr := s.repo.IsExistUserByID(ctx, userID)
+	if existErr != nil {
+		return UpdateGuestUserResponse{}, errlog.ErrContext(ctx, richerror.New(op).WithWrapError(existErr).
+			WithKind(richerror.KindUnexpected), s.logger)
+	}
+
+	if !exists {
+		return UpdateGuestUserResponse{}, richerror.New(op).WithMessage(servermsg.MsgRecordNotFound).WithKind(richerror.KindNotFound)
+	}
+
+	user, gErr := s.repo.GetUserByID(ctx, userID)
+	if gErr != nil {
+		return UpdateGuestUserResponse{}, errlog.ErrContext(ctx, richerror.New(op).WithWrapError(gErr).
+			WithKind(richerror.KindUnexpected), s.logger)
+	}
+
+	updateUser := UserUpdateFromSuperuserRequest{Username: user.Username, Roles: user.Roles}
+
+	if req.Fullname != "" {
+		updateUser.Fullname = req.Fullname
+	} else {
+		updateUser.Fullname = user.Fullname
+	}
+
+	if req.Email != "" {
+		updateUser.Email = req.Email
+	} else {
+		updateUser.Email = user.Email
+	}
+
+	if req.PhoneNumber != "" {
+		updateUser.PhoneNumber = req.PhoneNumber
+	} else {
+		updateUser.PhoneNumber = user.PhoneNumber
+	}
+
+	if uErr := s.repo.UpdateUser(ctx, userID, updateUser); uErr != nil {
+		return UpdateGuestUserResponse{}, errlog.ErrContext(ctx, richerror.New(op).WithWrapError(uErr).
+			WithKind(richerror.KindUnexpected), s.logger)
+	}
+
+	return UpdateGuestUserResponse{
+		User{
+			ID:             user.ID,
+			Username:       updateUser.Username,
+			HashedPassword: "",
+			Fullname:       updateUser.Fullname,
+			Email:          updateUser.Email,
+			PhoneNumber:    updateUser.PhoneNumber,
+			Avatar:         user.Avatar,
+			Roles:          user.Roles,
+			LastOnlineAt:   user.LastOnlineAt,
+		},
 	}, nil
 }
