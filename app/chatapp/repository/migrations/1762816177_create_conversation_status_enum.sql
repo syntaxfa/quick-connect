43b7d625d3e8661198383cfe928d2999cff6200a
alireza-fa <alirezafeyze44@gmail.com>
2025-11-28 19:54:05 +0330

    test(github): test sql-lint

--- app/chatapp/repository/migrations/1762816177_create_conversation_status_enum.sql
+++ app/chatapp/repository/migrations/1762816177_create_conversation_status_enum.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE TYPE conversation_status AS ENUM ('new', 'open', 'closed', 'bot_handling');
+CREATE TYPE conversation_status AS EnUM ('new', 'open', 'closed', 'bot_handling');
 
 -- +migrate Down
 DROP TYPE conversation_status;
