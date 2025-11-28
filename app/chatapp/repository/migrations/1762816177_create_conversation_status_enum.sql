b2a1c2d26590d82b2788d43d487bbb79e3a8dcb6
alireza-fa <alirezafeyze44@gmail.com>
2025-11-28 19:58:15 +0330

    fix(github): action-lint linter error

--- app/chatapp/repository/migrations/1762816177_create_conversation_status_enum.sql
+++ app/chatapp/repository/migrations/1762816177_create_conversation_status_enum.sql
@@ -1,5 +1,5 @@
 -- +migrate Up
-CREATE TYPE conversation_status AS EKHAM ("new", 'open', 'closed', 'bot_handling');
+CREATE TYPE conversation_status AS ENUM ('new', 'open', 'closed', 'bot_handling');
 
 -- +migrate Down
 DROP TYPE conversation_status;
