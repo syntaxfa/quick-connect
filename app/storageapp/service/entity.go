bd5b0e445707a0c1fc6355d1c8aa513727ed4843
alireza-fa <alirezafeyze44@gmail.com>
2025-12-16 22:38:53 +0330

    feat(storage): add file entity

--- app/storageapp/service/entity.go
+++ app/storageapp/service/entity.go
@@ -1,1 +1,29 @@
 package service
+
+import (
+	"time"
+
+	"github.com/syntaxfa/quick-connect/types"
+)
+
+type File struct {
+	ID         types.ID `json:"id"`
+	UploaderID types.ID `json:"uploader_id"`
+
+	Name    string `json:"name"`
+	Key     string `json:"key"`
+	MimType string `json:"mim_type"`
+	Size    int64  `json:"size"`
+
+	Driver   string `json:"driver"`
+	Bucket   string `json:"bucket"`
+	IsPublic bool   `json:"is_public"`
+
+	CreatedAt time.Time  `json:"created_at"`
+	UpdatedAt time.Time  `json:"updated_at"`
+	DeletedAt *time.Time `json:"deleted_at,omitempty"`
+}
+
+func (f File) IsDeleted() bool {
+	return f.DeletedAt != nil
+}
