387f0c5924e7177481127ad1a8195f197c9c78a3
alireza-fa <alirezafeyze44@gmail.com>
2025-12-19 03:34:05 +0330

    feat(storage): add internal grpc server

---
+++ app/storageapp/delivery/grpc/handler.go
@@ -0,0 +1,25 @@
+package grpc
+
+import (
+	"log/slog"
+
+	"github.com/syntaxfa/quick-connect/app/storageapp/service"
+	"github.com/syntaxfa/quick-connect/pkg/translation"
+	"github.com/syntaxfa/quick-connect/protobuf/storage/golang/storagepb"
+)
+
+type InternalHandler struct {
+	storagepb.UnimplementedStorageServiceServer
+
+	svc    service.Service
+	t      *translation.Translate
+	logger *slog.Logger
+}
+
+func NewInternalHandler(svc service.Service, t *translation.Translate, logger *slog.Logger) InternalHandler {
+	return InternalHandler{
+		svc:    svc,
+		t:      t,
+		logger: logger,
+	}
+}
